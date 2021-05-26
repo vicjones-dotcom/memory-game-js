6475b22703225d9de6fa195f3c4a8dc63843984f
vicjones-dotcom <79164908+vicjones-dotcom@users.noreply.github.com>
2021-05-26 23:06:20 +0300

    Created shuffle function

--- main.js
+++ main.js
@@ -3,8 +3,12 @@ const cards = document.querySelectorAll(".memory-card");
 let hasFlippedCard = false;
 
 let firstCard, secondCard;
+// Variable to avoid two sets of cards being turned at the same time
+let lockMemoryGame = false;
 
 function flipCard() {
+  // If a second card is clicked, lockMemoryGame === false
+  if (lockMemoryGame) return;
   this.classList.add("flip");
   // When no card is flipped -> hasFlippedCard = true & firstCard is set to clicked
   if (!hasFlippedCard) {
@@ -31,10 +35,21 @@ function disableCards() {
 }
 // To turn both cards back by removing "flip" class in 1500ms
 function unflipCards() {
+  // variable set to true->preventing any cards from flipping before the cards match or are hidden
+  lockMemoryGame = true;
   setTimeout(() => {
     firstCard.classList.remove("flip");
     secondCard.classList.remove("flip");
+    lockMemoryGame = false;
   }, 1500);
+}
+function shuffle() {
+  cards.forEach((card) => {
+    //   Math.floor -> rounds a number downwards to the nearest integer
+    // Looping through the cards -> generates a random number 0-12 -> assigns it to flex-item:order
+    let ramdom = Math.floor(Math.random() * 12);
+    card.style.order = ramdom;
+  });
 }
 // Looping through the cards and attaching event listener
 // when clicking on a card the flipCard function will be invoked
